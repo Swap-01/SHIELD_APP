64b9a2a3debdb5edde7f0302d3eb9204962560a9
Amit <daluiamit.19@gmail.com>
2023-05-19 17:53:56 +0530

    Initial Commit

---
+++ app/src/main/java/com/daluiamit/shield/MainActivity.java
@@ -0,0 +1,125 @@
+package com.daluiamit.shield;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    FusedLocationProviderClient fusedLocationProviderClient;
+    TextView lattitude,longitude,address,city,country;
+    Button getLocation;
+    private final static int REQUEST_CODE = 100;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        getLocation=findViewById(R.id.button);
+        getLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                getLastLocation();
+
+            }
+        });
+
+    }
+
+    private void getLastLocation(){
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED){
+
+
+            fusedLocationProviderClient.getLastLocation()
+                    .addOnSuccessListener(new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+
+                            if (location != null){
+
+
+
+                                try {
+                                    Geocoder geocoder = new Geocoder(MainActivity.this, Locale.getDefault());
+                                    List<Address> addresses = geocoder.getFromLocation(location.getLatitude(), location.getLongitude(), 1);
+//                                    lattitude.setText("Lattitude: "+addresses.get(0).getLatitude());
+//                                    longitude.setText("Longitude: "+addresses.get(0).getLongitude());
+//                                    address.setText("Address: "+addresses.get(0).getAddressLine(0));
+//                                    city.setText("City: "+addresses.get(0).getLocality());
+//                                    country.setText("Country: "+addresses.get(0).getCountryName());
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+
+
+                            }
+
+                        }
+                    });
+
+
+        }else {
+
+            askPermission();
+
+
+        }
+
+
+    }
+
+    private void askPermission() {
+
+        ActivityCompat.requestPermissions(MainActivity.this,new String[]{Manifest.permission.ACCESS_FINE_LOCATION},REQUEST_CODE);
+
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull @org.jetbrains.annotations.NotNull String[] permissions, @NonNull @org.jetbrains.annotations.NotNull int[] grantResults) {
+
+        if (requestCode == REQUEST_CODE){
+
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+
+
+                getLastLocation();
+
+            }else {
+
+
+                Toast.makeText(MainActivity.this,"Please provide the required permission",Toast.LENGTH_SHORT).show();
+
+            }
+
+
+
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+}
+
+
---
+++ app/src/main/java/com/daluiamit/shield/sendOTPActivity.java
@@ -0,0 +1,77 @@
+package com.daluiamit.shield;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.firebase.FirebaseException;
+import com.google.firebase.auth.PhoneAuthCredential;
+import com.google.firebase.auth.PhoneAuthProvider;
+
+import java.util.concurrent.TimeUnit;
+
+public class sendOTPActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_otpactivity);
+        final EditText inputMobile=findViewById(R.id.inputMobile);
+        Button buttonGetOTP=findViewById(R.id.buttonGetOTP);
+        final ProgressBar progressBar=findViewById(R.id.progressBar);
+
+        buttonGetOTP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (inputMobile.getText().toString().trim().isEmpty()) {
+                    Toast.makeText(sendOTPActivity.this, "Enter Mobile number", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                buttonGetOTP.setVisibility(View.INVISIBLE);
+
+                PhoneAuthProvider.getInstance().verifyPhoneNumber(
+                        "+91"+inputMobile.getText().toString(),
+                        60,
+                        TimeUnit.SECONDS,
+                        sendOTPActivity.this,
+                        new PhoneAuthProvider.OnVerificationStateChangedCallbacks(){
+                            @Override
+                            public void onVerificationCompleted(@NonNull PhoneAuthCredential phoneAuthCredential) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonGetOTP.setVisibility(View.VISIBLE);
+                            }
+
+                            @Override
+                            public void onVerificationFailed(@NonNull FirebaseException e) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonGetOTP.setVisibility(View.VISIBLE);
+                                Toast.makeText(sendOTPActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                            }
+
+                            @Override
+                            public void onCodeSent(@NonNull String verificationId, @NonNull PhoneAuthProvider.ForceResendingToken forceResendingToken) {
+                                progressBar.setVisibility(View.GONE);
+                                buttonGetOTP.setVisibility(View.VISIBLE);
+                                Intent intent=new Intent(getApplicationContext(),VerifyOTPActivity.class);
+                                intent.putExtra("mobile",inputMobile.getText().toString());
+                                intent.putExtra("verificationId",verificationId);
+                                startActivity(intent);
+                            }
+                        }
+
+
+                );
+
+            }
+        });
+
+    }
+}
